1b3bd8076a187ff42bf8bb855d99a8fd424b90d2
Albakhsh Sheikh <98633969+AlbakhshSheikh@users.noreply.github.com>
2025-04-16 20:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+  const btn = document.querySelector(`button:contains(${sound})`);
+  btn.addEventListener('click', () => {
+    stopSounds();
+    const audio = new Audio(`./sounds/${sound}.mp3`);
+    audio.play();
+  });
+});
+
+document.querySelector('.stop').addEventListener('click', stopSounds);
+
+function stopSounds() {
+  const audios = document.getElementsByTagName('audio');
+  Array.from(audios).forEach(audio => audio.pause());
+
+  // Also stop any newly created audio elements (playing sounds)
+  const tempAudio = document.querySelectorAll('audio');
+  tempAudio.forEach(el => el.remove());
+}
